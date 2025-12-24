a4743aca44509b3d4a6d15e6d4e78cafca18a524
Duhyun Kim <wooojin0316@naver.com>
2025-12-24 17:34:03 +0900

    플레이어 스킬 구현 완료
    1. 차원 방벽
    2. flow-style
    3. code-over_write
    구현 완료 및 플레이어 크기, 속도 조정

--- Assets/[6]Scripts/Gameplay/Player/PlayerShooter.cs
+++ Assets/[6]Scripts/Gameplay/Player/PlayerShooter.cs
@@ -38,22 +38,20 @@ public class PlayerShooter : MonoBehaviour
 
     private void Fire()
     {
-        // 1. 모드에 따라 프리팹 고르기
+        // 모드에 따라 프리팹 고르기
         GameObject prefabToUse = IsHomingMode ? homingBulletPrefab : normalBulletPrefab;
         if (prefabToUse == null) return;
 
-        // 2. 매니저에게 "이 프리팹 줘" 요청 (매니저가 없던 풀은 알아서 만듦)
         GameObject bulletObj = ObjectPoolManager.Instance.Spawn(prefabToUse, transform.position, Quaternion.identity);
 
         PlayerProjectile projectile = bulletObj.GetComponent<PlayerProjectile>();
         if (projectile != null)
         {
-            // [수정] projectile.SetOriginPrefab 호출 삭제 (매니저가 이미 함)
 
             int finalDamage = stats.AttackPower;
             if (IsHomingMode) finalDamage = Mathf.Max(1, finalDamage / 2);
 
-            // 3. 총알 초기화 (유도 여부 전달)
+            // 총알 초기화
             projectile.Initialize(Vector2.up, finalDamage, IsHomingMode);
         }
     }
